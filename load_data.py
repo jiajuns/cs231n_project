8d9e70aa0423be116f3b6e2215e058f3dc74c9bc
jingw2 <jingwangtju@gmail.com>
2017-05-11 11:22:12 -0700

    update imageio module plugin, update to save category and update frame count and class initialization

--- load_data.py
+++ load_data.py
@@ -1,15 +1,17 @@
 
-#!usr/env/python 2.7
+#!usr/env/python 3.5
 '''
 Load Data
 
-One minute per video, One frame per minute
+set download num
+set output num 
+set video time 
 
 Output:
 - videos folder: original downloaded videos
 - processed folder: processed videos with shorter time
 - frames folder: many frames subfolders
-- category.npy: each category label
+- category.npy: each video category
 '''
 
 import os
@@ -18,13 +20,15 @@ from datetime import datetime
 from pytube import YouTube
 from pprint import pprint
 import imageio
+# download plugins
+imageio.plugins.ffmpeg.download()
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import cv2
 import numpy as np
 
 class Download_Video(object):
 
-    def __init__(self, download_num=10):
+    def __init__(self, download_num=10, output_num = 10, video_time = 60):
         # current path
         self.curr = os.getcwd()
         self.train_path = os.path.join(self.curr, 'datasets/train_2017/videodatainfo_2017.json')
@@ -33,9 +37,10 @@ class Download_Video(object):
             f.close()
 
         self.download_num = download_num
+
         # frame parameters
-        self.output_frames = 60
-        self.video_time = 60
+        self.output_frames = output_num
+        self.video_time = video_time
 
 
     def download(self):
@@ -58,13 +63,13 @@ class Download_Video(object):
             if not os.path.exists(d1):
                 os.makedirs(d1)
 
-            # vid.set_filename(idx)
             file_path = os.path.join(d1, idx)
             # if file already exists skip download
             if not os.path.isfile(file_path):
                 vid.download(d1)
-
+            print('*'*30)
             print('Finish downloading {0}'.format(idx))
+            print('*'*30)
 
             download_count += 1
             if download_count >= self.download_num:
@@ -73,14 +78,17 @@ class Download_Video(object):
 
     def preprocess(self):
         # cut video clip
+        Y = []
         download_count = 0
         for video_idx, video in enumerate(self.train['videos']):
 
             # get video information
             category = video['category']
             idx = video['video_id']
+
             # save category label
-            # Y.append(category)
+            Y.append(category)
+
             url = video['url']
             start_time = video['start time']
             end_time = video['end time']
@@ -100,9 +108,9 @@ class Download_Video(object):
             if os.path.isfile(tname):
                 download_count += 1
                 if download_count >= self.download_num:
-                    print('Finish processing {0}'.format(idx))
+                    print('Finish preprocess Video {0}'.format(idx))
                     break
-                print('Finish processing {0}'.format(idx))
+                print('Finish preprocess Video {0}'.format(idx))
                 continue
 
             ffmpeg_extract_subclip(oname, start_time, end_time, targetname=tname)
@@ -115,7 +123,7 @@ class Download_Video(object):
             while success:
                 success, image = vidcap.read()
                 tot_count += 1
-            # read again, totally output 20 frames
+
             output_interval = tot_count // self.output_frames
 
             # how many frames per second
@@ -124,25 +132,29 @@ class Download_Video(object):
             vidcap = cv2.VideoCapture(tname)
             success,image = vidcap.read()
             count = 0
+            frame_count = 0
             while success:
                 success,image = vidcap.read()
                 count += 1
                 if count % output_interval == 0: # per second
+
+                    # check the number of output frames
+                    if frame_count == self.output_frames:
+                        break
+                    frame_count += 1
                     directory = os.path.join(self.curr, 'datasets/frames', idx)
                     if not os.path.exists(directory):
                         os.makedirs(directory)
                     cv2.imwrite(os.path.join(directory, "frame%d.jpg" % count), image)
 
-            print('Finish processing {0}'.format(idx))
-
+            print('*'*30)
+            print('Finish processing Video {0}'.format(idx))
+            print('*'*30)
 
             download_count += 1
             if download_count >= self.download_num:
                 break
 
-            # # to simply check codes availability
-            # if video_idx == :
-            #     break
-
-        # output_y_dir = curr + '/datasets/' + 'category.npy'
-        # np.save(output_y_dir, np.array(Y))
+        # save category
+        output_y_dir = self.curr + '/datasets/category.npy'
+        np.save(output_y_dir, np.array(Y))
