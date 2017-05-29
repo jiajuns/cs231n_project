9b8873309ce4b03c6f4c4598a894129bd26071f2
tingchunyeh <ytchun18@gmail.com>
2017-05-29 22:48:10 +0000

    dict update

--- util.py
+++ util.py
@@ -27,32 +27,6 @@ def minibatch(input_frames, captions, batch_size):
 
     return (batch_input_frames, batch_input_captions)
 
-def index_word_dict(vocDir):
-    '''
-    Input: 
-    - vocDir (str) vocabulary pickle directory
-
-    Output:
-    - dict (dictionary) word to index and index to word
-    '''
-    try:
-
-
-def index2Word(wordls, vocabulary):
-    '''
-    Input: 
-    - wordls: (list) a list of word 
-    - vocabulary: (pickle file)
-
-    Index to Word 
-
-    Output:
-    - indexls: (list) indice for word list
-    '''
-    try: 
-        voc = pickle.dump()
-    l = len(vocabulary)
-    dic = {word: index for word, index in zip(voc.keys(), range(l))}
 
 
 def build_word_to_index_dict():
@@ -84,20 +58,55 @@ def build_word_to_index_dict():
     with open(dataPath+'index_to_word.pickle', 'wb') as handle:
         pickle.dump(ind2w_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
-
-def build_caption_data():
+def caption_to_ind(caption_split, w2ind_dict, maxLen = 20):
+    res = []
+    for i, word in enumerate(caption_split):
+        wordInd = w2ind_dict.get(word)
+        res.append(wordInd)
+    
+    n = len(res)
+    for j in range(n, maxLen, 1):
+        res.append(None)
+        
+    return res
+        
+def build_caption_data(maxLen = 20):
     curPath = os.getcwd()
     dataPath = curPath + "/datasets/train_2017/"
-    
     w2ind = pickle.load(open(dataPath+"word_to_index.pickle", "rb"))
     id_cap_dict = pickle.load(open(dataPath+"id_caption_dict.pickle", "rb"))
 
+    
     caption_data = []
     for video_id, captionLs in id_cap_dict.items():
         video_id = video_id[5:]
+        
         for caption in captionLs:
-            video_caption_pair = tuple([int(video_id), caption])
+            caption_split = caption.split()
+            
+            if len(caption_split) > maxLen: # only take captions within maxLen
+                break
+            
+            captionInd = list(caption_to_ind(caption_split, w2ind, maxLen))
+            video_caption_pair = tuple([int(video_id), captionInd])
             caption_data.append(video_caption_pair)
-    
+            
     with open(dataPath+'video_caption_pairLs.pickle', 'wb') as handle:
         pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+        
+def check_caption_data(caption_id):
+    curPath = os.getcwd()
+    dataPath = curPath + "/datasets/train_2017/"
+    caption_data = pickle.load(open(dataPath+"video_caption_pairLs.pickle", "rb"))
+    ind2w = pickle.load(open(dataPath+"index_to_word.pickle", "rb"))
+    
+    print((caption_data[caption_id]))
+    for ind in caption_data[caption_id][1]:
+        if ind==None:continue
+        print (ind2w[ind])
+
+if __name__ == "__main__":
+    #build_word_to_index_dict()
+    #build_caption_data(20)
+    check_caption_data(50)
