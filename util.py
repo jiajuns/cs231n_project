48353e376f240031b3295753b31364bab401dc94
tingchun <ytchun18@gmail.com>
2017-05-30 15:33:40 -0700

    update id_captionInd_train and id_captionInd_test. (index: 0~xxxx)

--- util.py
+++ util.py
@@ -35,10 +35,7 @@ def minibatches(input_frames, captions, batch_size, max_len):
 
 
 
-def build_word_to_index_dict():
-    curPath = os.getcwd()
-    dataPath = curPath + "/datasets/"
-
+def build_word_to_index_dict(dataPath):
     w2v = pickle.load(open(dataPath+"word2Vector.pickle", "rb"))
     id_cap_dict = pickle.load(open(dataPath+"id_caption_dict.pickle", "rb"))
 
@@ -47,7 +44,6 @@ def build_word_to_index_dict():
 
     w2ind_dict = {}
     ind2w_dict = {}
-
     for ind, word in enumerate(w2v_keys_sorted):
         w2ind_dict[word] = ind
         ind2w_dict[ind] = word
@@ -57,7 +53,6 @@ def build_word_to_index_dict():
     # store
     with open(dataPath+'word2index.pickle', 'wb') as handle:
         pickle.dump(w2ind_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
     with open(dataPath+'index2word.pickle', 'wb') as handle:
         pickle.dump(ind2w_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -71,25 +66,20 @@ def caption_to_ind(caption_split, w2ind_dict, maxLen = 20):
         if len(res) >= 4:
             return res
 
-
-
     n = len(res)
     for j in range(n, maxLen, 1):
         res.append(None)
 
     return res
 
-def build_caption_data(maxLen = 20):
-    curPath = os.getcwd()
-    dataPath = curPath + "/datasets/"
+def build_caption_data(dataPath, id_Ls, maxLen = 20):
     w2ind = pickle.load(open(dataPath+"word2index.pickle", "rb"))
     id_cap_dict = pickle.load(open(dataPath+"id_caption_dict.pickle", "rb"))
 
 
     caption_data = []
-    for video_id, captionLs in id_cap_dict.items():
-        video_id = video_id[5:]
-
+    for count, video_id in enumerate(id_Ls):
+        captionLs = id_cap_dict["video"+str(video_id)]
         for caption in captionLs:
             caption_split = ["<START>"] + caption.split()
 
@@ -97,16 +87,11 @@ def build_caption_data(maxLen = 20):
             #     break
 
             captionInd = list(caption_to_ind(caption_split, w2ind, maxLen))
-            video_caption_pair = tuple([int(video_id), captionInd])
+            video_caption_pair = tuple([int(count), captionInd])
             caption_data.append(video_caption_pair)
+    return caption_data
 
-    with open(dataPath+'id_captionInd_pairs.pickle', 'wb') as handle:
-        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def check_caption_data(caption_id):
-    curPath = os.getcwd()
-    dataPath = curPath + "/datasets/"
+def check_caption_data(dataPath, caption_id):
     caption_data = pickle.load(open(dataPath+"id_captionInd_pairs.pickle", "rb"))
     ind2w = pickle.load(open(dataPath+"index2word.pickle", "rb"))
 
@@ -115,19 +100,33 @@ def check_caption_data(caption_id):
         if ind==None:continue
         print (ind2w[ind])
 
+
+
 if __name__ == "__main__":
     curPath = os.getcwd()
     dataPath = curPath + "/datasets/"
-    # build_word_to_index_dict()
+    # build_word_to_index_dict(dataPath)
     # build_caption_data(4)
-    # check_caption_data(4000)
+    # check_caption_data(dataPath, 4000)
 
     ind2w = pickle.load(open(dataPath+"index2word.pickle", "rb"))
     w2ind = pickle.load(open(dataPath+"word2index.pickle", "rb"))
     captions = pickle.load(open(dataPath+"id_captionInd_pairs.pickle", "rb"))
+    x_train_ind = np.load(dataPath+'x_train_ind_above400.npy')
+    x_test_ind = np.load(dataPath+'x_test_ind_above400.npy')
 
-    for key,values in captions:
-        print(key,  values, ":     "," ".join([ind2w[i] for i in values ]))
+    caption_data = build_caption_data(dataPath, x_train_ind, 4)
+    with open(dataPath+'id_captionInd_train.pickle', 'wb') as handle:
+        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    caption_data = build_caption_data(dataPath, x_test_ind, 4)
+    with open(dataPath+'id_captionInd_test.pickle', 'wb') as handle:
+        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+    # for key,values in captions:
+    #     print(key,  values, ":     "," ".join([ind2w[i] for i in values ]))
 
 
     # input_frames = np.random.randn(100, 15, 7, 7, 512)
