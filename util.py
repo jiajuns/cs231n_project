975b4f5ccbe36a3eef5cc8c3989b56824bacfce8
tingchun <ytchun18@gmail.com>
2017-05-30 16:32:22 -0700

    revised util.py and upate pickles (pad with <unk> instead of None)

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import numpy as np
 import pickle
 import os
 
-
 def minibatches(input_frames, captions, batch_size, max_len):
     '''
     Input:
@@ -33,22 +32,43 @@ def minibatches(input_frames, captions, batch_size, max_len):
         count += 1
     return (batch_input_frames, batch_input_captions)
 
+class ind_word_convertor():
+    '''
+    class ind_word_convertor() provides function to do convertion between
+    word and index
+    '''
 
+    def __init__(self):
+        dataPath = os.getcwd() + "/datasets/"
+        self.w2ind = pickle.load(open(dataPath+"word2index.pickle", "rb"))
+        self.ind2w = pickle.load(open(dataPath+"index2word.pickle", "rb"))
+
+    def word_to_index(self, word):
+        '''
+        input: the word
+        reutrn: the corresponding index
+        '''
+        ind = self.w2ind[word]
+        return ind
+
+    def index_to_word(self, index):
+        '''
+        input: index of word
+        reutrn: the corresponding word
+        '''
+        word = self.ind2w[index]
+        return word
 
 def build_word_to_index_dict(dataPath):
     w2v = pickle.load(open(dataPath+"word2Vector.pickle", "rb"))
+    w2v_keys_sorted = sorted(list(w2v.keys()))
     id_cap_dict = pickle.load(open(dataPath+"id_caption_dict.pickle", "rb"))
 
-    w2v_keys = list(w2v.keys())
-    w2v_keys_sorted = sorted(w2v_keys)
 
-    w2ind_dict = {}
-    ind2w_dict = {}
+    w2ind_dict, ind2w_dict= {}, {}
     for ind, word in enumerate(w2v_keys_sorted):
         w2ind_dict[word] = ind
         ind2w_dict[ind] = word
-    ind2w_dict[None] = "<pad>"
-    w2ind_dict["<pad>"] = None
 
     # store
     with open(dataPath+'word2index.pickle', 'wb') as handle:
@@ -56,26 +76,22 @@ def build_word_to_index_dict(dataPath):
     with open(dataPath+'index2word.pickle', 'wb') as handle:
         pickle.dump(ind2w_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def caption_to_ind(caption_split, w2ind_dict, maxLen = 20):
-    res = []
-    for i, word in enumerate(caption_split):
-        wordInd = w2ind_dict.get(word, w2ind_dict["<unk>"])
-        res.append(wordInd)
 
-        # temp code for test
-        if len(res) >= 4:
-            return res
+def build_caption_data_dict(dataPath):
+    x_train_ind = np.load(dataPath+'x_train_ind_above400.npy')
+    x_test_ind = np.load(dataPath+'x_test_ind_above400.npy')
+    caption_data = cpation_data(dataPath, x_train_ind, 4)
+    with open(dataPath+'id_captionInd_train.pickle', 'wb') as handle:
+        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    n = len(res)
-    for j in range(n, maxLen, 1):
-        res.append(None)
+    caption_data = cpation_data(dataPath, x_test_ind, 4)
+    with open(dataPath+'id_captionInd_test.pickle', 'wb') as handle:
+        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    return res
 
-def build_caption_data(dataPath, id_Ls, maxLen = 20):
+def cpation_data(dataPath, id_Ls, maxLen = 20):
     w2ind = pickle.load(open(dataPath+"word2index.pickle", "rb"))
     id_cap_dict = pickle.load(open(dataPath+"id_caption_dict.pickle", "rb"))
-
 
     caption_data = []
     for count, video_id in enumerate(id_Ls):
@@ -83,51 +99,39 @@ def build_caption_data(dataPath, id_Ls, maxLen = 20):
         for caption in captionLs:
             caption_split = ["<START>"] + caption.split()
 
-            # if len(caption_split) > maxLen: # only take captions within maxLen
-            #     break
+            if len(caption_split) > maxLen: # only take captions within maxLen
+                break
 
             captionInd = list(caption_to_ind(caption_split, w2ind, maxLen))
             video_caption_pair = tuple([int(count), captionInd])
             caption_data.append(video_caption_pair)
     return caption_data
 
-def check_caption_data(dataPath, caption_id):
-    caption_data = pickle.load(open(dataPath+"id_captionInd_pairs.pickle", "rb"))
-    ind2w = pickle.load(open(dataPath+"index2word.pickle", "rb"))
 
-    print((caption_data[caption_id]))
-    for ind in caption_data[caption_id][1]:
-        if ind==None:continue
-        print (ind2w[ind])
+def caption_to_ind(caption_split, w2ind_dict, maxLen = 20):
+    res = []
+    for i, word in enumerate(caption_split):
+        wordInd = w2ind_dict.get(word, w2ind_dict["<unk>"])
+        res.append(wordInd)
 
+    # pad the rest length with <unk>
+    n = len(res)
+    for j in range(n, maxLen, 1):
+        res.append(w2ind_dict["<unk>"])
+    return res
 
 
 if __name__ == "__main__":
-    curPath = os.getcwd()
-    dataPath = curPath + "/datasets/"
+    dataPath = os.getcwd() + "/datasets/"
+
+    # build dictionary pickles
     # build_word_to_index_dict(dataPath)
-    # build_caption_data(4)
-    # check_caption_data(dataPath, 4000)
+    # build_caption_data_dict(dataPath)
 
     ind2w = pickle.load(open(dataPath+"index2word.pickle", "rb"))
     w2ind = pickle.load(open(dataPath+"word2index.pickle", "rb"))
-    captions = pickle.load(open(dataPath+"id_captionInd_pairs.pickle", "rb"))
-    x_train_ind = np.load(dataPath+'x_train_ind_above400.npy')
-    x_test_ind = np.load(dataPath+'x_test_ind_above400.npy')
-
-    caption_data = build_caption_data(dataPath, x_train_ind, 4)
-    with open(dataPath+'id_captionInd_train.pickle', 'wb') as handle:
-        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    caption_data = build_caption_data(dataPath, x_test_ind, 4)
-    with open(dataPath+'id_captionInd_test.pickle', 'wb') as handle:
-        pickle.dump(caption_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    
-
-    # for key,values in captions:
-    #     print(key,  values, ":     "," ".join([ind2w[i] for i in values ]))
-
+    caption_train = pickle.load(open(dataPath+"id_captionInd_train.pickle", "rb"))
+    caption_test = pickle.load(open(dataPath+"id_captionInd_test.pickle", "rb"))
 
     # input_frames = np.random.randn(100, 15, 7, 7, 512)
     #
